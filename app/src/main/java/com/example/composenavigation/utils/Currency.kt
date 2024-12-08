72db7b7fa60a4b11ba213b4b4fbf9aabc6584792
ritesh bagul <ritesh.sonar111@gmail.com>
2024-12-08 23:39:21 +0530

    Add drawable file

---
+++ app/src/main/java/com/example/composenavigation/utils/Currency.kt
@@ -0,0 +1,8 @@
+package com.example.composenavigation.utils
+
+import java.math.BigDecimal
+import java.text.NumberFormat
+
+fun formatPrice(price: Long): String {
+    return NumberFormat.getCurrencyInstance().format(BigDecimal(price).movePointLeft(2))
+}
